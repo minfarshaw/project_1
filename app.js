667e6a32f49cb6e4351829c8fcbbaa7f001626d9
Amanda Hayes <amandahayes@amandas-mbp.lan>
2020-11-18 11:36:00 -0800

    refactored significantly. created the first modal

--- app.js
+++ app.js
@@ -1,75 +1,78 @@
 console.log('Yes Im connected');
 // CACHED DOM NODES
 
-// MODAL with the welcome message and RULES
-// Button to begin the game
-
-// Make a class constructor for PLAYER
-// Add functions needed 
-
-
-// Update HP in DOM 
-
-
-// MODAL for intro from Mark
-// it includes a button to start the ride or button to quit
-// It will have a background GIF or image of MARK the hellivator operator
-
-
-// Make the button take you to another MODAL with the floor you've arrived on
-// Make the floor you arrive on populate randomly
-// Make Mark give some information
-// Do not include David pumpkins in the first two rides 
-// create photo with the scary person on the modal & their catchphrase
-// calculate amount of points earned based on the floor
-// Regular floor: Win 5 points
-// David floor: Lose 6 points
-// Log the points in the DOM
-// add buttons for riding again, answering a question, or quit
-// If clicked RIDE, repeat this modal with a NEW image
-
-// Make the modal for the trivia round
-// Make the trivia questions populate randomly (array?)
-// Make submit button functionality that sends to resolution modal
-
-
-// Make the resolution modal
-// Create the functionality for the answer being correct/incorrect
-// If correct, award points
-// If incorrect, do nothing
-// Do NOT offer to ask another question after this
-// Give button options of RIDE or QUIT
-// Update the players points back on the homepage
-// This code will not run until after the modal is closed, 
-// bringing up another modal will mean the points aren't tabulated on homepage?
-
-// Make the modal for David Pumpkins appearance
-// Do not start until at least 2 rounds, then appear more often (more Davids in the array?)
-
-// Make Modal for game ending scenario
-// If functionality that determines if user has gone through all floors, STOP game
-// If player has points, then user wins
-// Go to congratulations/prize announcement
-// Make option to start over(ride again)
-
-// If player does not get to all floors, they lose
-// As soon as player hits zero points, game ends
-// Go to sad announcement and say player fainted with fear image
-// Make option to start the game over(ride again)r
-
-// Make modal for SURPRISE ROUND
-// Make this not possible until at least 10 floors
-// Make only David appear
-// Lose 10 points
-// Make Game STOP
-// Calculate total
-// Ask If you want to FACE David P
-// Or run away? (quit)
-
-
-// Face David: Modal with Congratulations
-// You win
-// Final point total
+const beginButton = document.querySelector(".begin-game");
+const letsGoBtn = document.querySelector('.lets-go');
 
 
 
+class Player {
+    constructor (name, hitpoints) {
+        this.name = name;
+        this.hitpoints = hitpoints;
+    }
+}
+
+class Floor {
+    constructor (name, points) {
+        this.name = name;
+        this.points = points;
+    }
+}
+
+/* =============================
+FUNCTIONS
+============================= */
+
+
+const randomInt = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+
+/* =============================
+EVENT LISTENERS
+============================= */
+
+
+
+/* ======================
+GLOBAL VARS
+=========================*/
+
+const firstPlayer = new Player("Player", 0);
+
+const floor41 = new Floor("Corpse Bride", 5);
+
+
+
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
