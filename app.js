116e7ce2f63e949d5ab0263bf13f81d8b0528f5c
Amanda Hayes <amandahayes@amandas-mbp.lan>
2020-11-18 12:12:28 -0800

    Got double modals all the way across the page. yeah yeah so intense

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 console.log('Yes Im connected');
 // CACHED DOM NODES
 
-const beginButton = document.querySelector(".begin-game");
-const letsGoBtn = document.querySelector('.lets-go');
-
 
 
 class Player {
@@ -51,28 +48,34 @@ const floor41 = new Floor("Corpse Bride", 5);
 
 
 // Get the modal
-const modal = document.getElementById("myModal");
-
+const modal = document.getElementsByClassName('modal');
 
 // Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+const btn = document.getElementsByClassName("myBtn");
+
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal 
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
 }
 
+btn[1].onclick = function() {
+    modal[1].style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span[0].onclick = function() {
+    modal[0].style.display = "none";
 }
 
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
