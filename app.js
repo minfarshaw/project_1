0d0507c0616dd2e5164d2d729495ed50098102b0
Amanda Hayes <amandahayes@amandas-mbp.lan>
2020-11-18 16:47:08 -0800

    created a universal modal. created functions that opens the modal and returns a random floor with message

--- app.js
+++ app.js
@@ -2,18 +2,46 @@ console.log('Yes Im connected');
 // CACHED DOM NODES
 
 
-
 class Player {
-    constructor (name, hitpoints) {
+    constructor (name, points) {
         this.name = name;
-        this.hitpoints = hitpoints;
+        this.points = points;
+    }
+
+    getName () {
+        return this.name;
+    }
+
+    getPoints () {
+        return this.points;
+    }
+
+    addPoints (pointsToAdd) {
+        this.points += pointsToAdd;
+    }
+
+    removePoints (pointsToRemove) {
+        this.points -= pointsToRemove;
     }
 }
 
 class Floor {
-    constructor (name, points) {
+    constructor (floor, name, points) {
+        this.floor = floor;
         this.name = name;
         this.points = points;
+    }
+
+    getFloorNumber () {
+        return this.floor;
+    }
+
+    getFloorName () {
+        return this.name;
+    }
+
+    getFloorPoints () {
+        return this.points;
     }
 }
 
@@ -28,11 +56,15 @@ const randomInt = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// console.log(randomInt(1,15));
+
+ function visitFloor() {
+    return randomInt(1, 15); 
+}
 
 
-/* =============================
-EVENT LISTENERS
-============================= */
+
+
 
 
 
@@ -42,40 +74,53 @@ GLOBAL VARS
 
 const firstPlayer = new Player("Player", 0);
 
-const floor41 = new Floor("Corpse Bride", 5);
+const floor41 = new Floor(41, "Corpse Bride", 5);
+
+const floor79 = new Floor(71, "Chainsaw Lady", 5)
 
 
 
 
 // Get the modal
-const modal = document.getElementsByClassName('modal');
+const modal = document.getElementById("myModal");
+const modalBody = document.getElementsByClassName("modal-body")[0];
 
 // Get the button that opens the modal
-const btn = document.getElementsByClassName("myBtn");
-
+const rideButton = document.getElementById("rideBtn");
+const newGameButton = document.getElementById("newGameBtn");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close");
+const closeSpan = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn[0].onclick = function() {
-    modal[0].style.display = "block";
+rideButton.onclick = function() {
+    modal.style.display = "block";
+    modalBody.innerText = floorArrival();
 }
 
-btn[1].onclick = function() {
-    modal[1].style.display = "block";
+function floorArrival () {
+    let arrivalMessage = "You have arrived at floor " + visitFloor();
+    return arrivalMessage += " So scary!";
 }
+
+newGameButton.onclick = function() {
+    modal.style.display = "block";
+    modalBody.innerText = "moooooooooo";
+}
+
 // When the user clicks on <span> (x), close the modal
-span[0].onclick = function() {
-    modal[0].style.display = "none";
+closeSpan.onclick = function() {
+    modal.style.display = "none";
 }
 
-span[1].onclick = function() {
-    modal[1].style.display = "none";
-}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+// modal[1].on('open', function () {
+//     console.log("I fookin did it");
+//   });
