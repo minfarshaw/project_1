f93b23283313e250a0ec4bf71fe47156d09a4a6f
Amanda Hayes <amandahayes@amandas-mbp.lan>
2020-11-19 22:01:09 -0800

    3 functioning modals. Made Mark give advice. Made framework for trivia. Refactored. Styled.

--- app.js
+++ app.js
@@ -1,5 +1,75 @@
 console.log('Yes Im connected');
 // CACHED DOM NODES
+const modal = document.getElementById("myModal");
+const modalBody = document.getElementsByClassName("modal-body")[0];
+const img = document.getElementById("myImg");
+
+const rideButton = document.getElementById("rideBtn");
+const newGameButton = document.getElementById("newGameBtn");
+const getAdviceButton = document.getElementById("getAdviceBtn");
+const answerQuestionButton = document.getElementById("answerQuestionBtn");
+
+const closeSpan = document.getElementsByClassName("close")[0];
+const closeButton = document.getElementsByClassName("close-button")[0];
+
+const currentStatsContainer = document.querySelector('.life-stats');
+
+const frightGameHome = document.querySelector("#fright-Game-home")
+
+const floors = [
+    {
+        image: src="images/david-pumpkins.png",
+        name: "David Pumpkins",
+        points: 5,
+        floorNumber: 10,
+        catchphrase: "'How's it hangin'? I'm David Pumpkins, and I'm here to scare the HELL out of YOU!'"
+    },
+    {
+        image: src="images/corpse-bride.png",
+        name: "Corpse Bride",
+        points: 5,
+        floorNumber: 46,
+        catchphrase: "'Winifred Mancheseter got cold feet, and hung herself in the honeymoon suiiiiiite!'"
+    },
+    {
+        image: src="images/chainsaw-lady.png",
+        name: "Chainsaw Lady",
+        points: 5,
+        floorNumber: 97,
+        catchphrase: "'I'M CRAZY!'"
+    },
+    {
+        image: src="images/decapitated-waiter.png",
+        name: "Decapitating Waiter",
+        points: 5,
+        floorNumber: 12,
+        catchphrase: "'Tonight's Special....YOUR HEAD!!!'"
+    },
+    {
+        image: src="images/the-ring-girl.png",
+        name: "Girl from The Ring",
+        points: 5,
+        floorNumber: 29,
+        catchphrase: "'Can I sleep in your bed tonight?'"
+    },
+  ];
+
+ const advice = [
+    "Did you know this place used to be a movie studio? Crazy!",
+    
+    "My favorite floor is the one where you watch your hopes and dreams smashed to bits by Gritty",
+    
+    "I think these emotional support companions have gone too far. One lady brought in a goldfish. Carried the tank around and everything!",
+    
+    "This hotel has a 2 star rating on Yelp!",
+
+    "Did you know my last name is yMarkandthefunkybunch? I wish I knew where it came from.",
+
+    "I can't wait til I'm off. My friends and I are gonna have a viewing party for the new Paul Blart movie!",
+
+    "You're not supposed to eat in here, but I brought in some deviled eggs. Hungry?",
+    
+  ];
 
 
 class Player {
@@ -12,17 +82,31 @@ class Player {
         return this.name;
     }
 
+
     getPoints () {
         return this.points;
     }
 
     addPoints (pointsToAdd) {
         this.points += pointsToAdd;
+      
     }
 
     removePoints (pointsToRemove) {
         this.points -= pointsToRemove;
+  
     }
+
+    // // // Update points in DOM 
+    // updateCurrentPoints() {
+    // currentStatsContainer.innerHTML = `
+    // <div> Get Points </div>
+    // `
+    //  fright-Game-home.appendChild(currentStatsContainer);
+    // }
+      
+    //  
+ 
 }
 
 class Floor {
@@ -49,24 +133,26 @@ class Floor {
 FUNCTIONS
 ============================= */
 
+ function getFloor() {
 
-const randomInt = (min, max) => {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+    const randomNumber = Math.floor(Math.random() * Math.floor(floors.length-1));
+    // console.log(randomNumber);
+    return floors[randomNumber];
 
-// console.log(randomInt(1,15));
-
- function visitFloor() {
-    return randomInt(1, 15); 
 }
 
+function getAdvice() {
 
+    const randomNumber = Math.floor(Math.random() * Math.floor(advice.length-1));
+    // console.log(randomNumber);
+    // Set my function to pull from the array of objects
+    return advice[randomNumber];
+    
+}
 
+function answerQuestion() {
 
-
-
+}
 
 /* ======================
 GLOBAL VARS
@@ -74,42 +160,47 @@ GLOBAL VARS
 
 const firstPlayer = new Player("Player", 0);
 
-const floor41 = new Floor(41, "Corpse Bride", 5);
-
-const floor79 = new Floor(71, "Chainsaw Lady", 5)
-
-
-
-
-// Get the modal
-const modal = document.getElementById("myModal");
-const modalBody = document.getElementsByClassName("modal-body")[0];
-
-// Get the button that opens the modal
-const rideButton = document.getElementById("rideBtn");
-const newGameButton = document.getElementById("newGameBtn");
-
-// Get the <span> element that closes the modal
-const closeSpan = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-rideButton.onclick = function() {
-    modal.style.display = "block";
-    modalBody.innerText = floorArrival();
-}
-
-function floorArrival () {
-    let arrivalMessage = "You have arrived at floor " + visitFloor();
-    return arrivalMessage += " So scary!";
-}
 
 newGameButton.onclick = function() {
     modal.style.display = "block";
-    modalBody.innerText = "moooooooooo";
+    modalBody.innerHTML = 
+    `<h1>Welcome to 100 Floors of Frights!</h1>
+    <p>Here are the basic rules</p>`;
 }
+
+rideButton.onclick = function() {
+    modal.style.display = "block";
+    modalBody.innerHTML = floorArrival();
+    
+}
+
+getAdviceButton.onclick = function() {
+    modal.style.display = "block";
+    modalBody.innerText = "Mark: " + getAdvice();
+}
+
+answerQuestionButton.onclick = function() {
+    modal.style.display = "block";
+    modalBody.innerText = "Mark: " + ();
+}
+
+function floorArrival () {
+    let floor = getFloor();
+    firstPlayer.addPoints(5);
+    let arrivalMessage = "You have arrived at floor " + floor.floorNumber + ". " + floor.image;
+    return arrivalMessage += " " + floor.catchphrase + " " + " You have earned " + floor.points + " points!";
+    // Now give me my points
+    
+    
+}   
 
 // When the user clicks on <span> (x), close the modal
 closeSpan.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks OK, close the modal
+closeButton.onclick = function() {
     modal.style.display = "none";
 }
 
@@ -121,6 +212,3 @@ window.onclick = function(event) {
     }
 }
 
-// modal[1].on('open', function () {
-//     console.log("I fookin did it");
-//   });
